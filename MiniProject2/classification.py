edab37d38a2ca4d536a2f3852ba8a869c5e57d0f
ZhuyuLi <zhuyu.li@foxmail.com>
2018-10-23 05:45:16 -0400

    main program finished, need one more model

--- MiniProject2/classification.py
+++ MiniProject2/classification.py
@@ -3,92 +3,115 @@ import os
 import cv2
 from keras.models import Sequential,load_model
 from keras.layers import *
+from keras.preprocessing.image import img_to_array
 from keras.callbacks import TensorBoard
 from keras.utils import plot_model
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers import Dropout
+from keras.callbacks import ReduceLROnPlateau, CSVLogger, EarlyStopping, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.optimizers import Adam
+
+
+def extract_data(file_path, norm_size):
+
+    image_data = []
+    label_data = []
+
+    for data_catalog in os.listdir(file_path):
+        for each_image in os.listdir(file_path + '/' + data_catalog):
+            img_read = cv2.imread(file_path + '/' + data_catalog + '/' + each_image, cv2.IMREAD_GRAYSCALE)
+            img_read = cv2.resize(img_read, (norm_size, norm_size), interpolation=cv2.INTER_CUBIC)
+            img_read = img_to_array(img_read)
+
+            image_data.append(img_read)
+            label_data.append(data_catalog)
+
+    image_data = np.array(image_data, dtype="float") / 255.0
+    label_data = np.array(label_data)
+    label_data = to_categorical(label_data, num_classes=2)
+
+    print(image_data.shape)
+    print(label_data.shape)
+    return image_data, label_data
 
 
 
-def extract_data(file_path):
-
-    train_image_raw_data = []
-    test_image_raw_data = []
-    train_label_data = []
-    test_label_data = []
-
-    train_sub_path = '/dataset/train/'
-    test_sub_path = '/dataset/test/'
-
-    for data_catalog in os.listdir(file_path + train_sub_path):
-        for each_image in os.listdir(file_path + train_sub_path + data_catalog):
-            train_image_raw_data.append(cv2.resize(
-                cv2.imread(file_path + train_sub_path + data_catalog + '/' + each_image, cv2.IMREAD_GRAYSCALE), (64, 64),
-                interpolation=cv2.INTER_CUBIC))
-            train_label_data.append(data_catalog)
-
-    for data_catalog in os.listdir(file_path + test_sub_path):
-        for each_image in os.listdir(file_path + test_sub_path + data_catalog):
-            test_image_raw_data.append(cv2.resize(cv2.imread(file_path + test_sub_path + data_catalog + '/' + each_image, cv2.IMREAD_GRAYSCALE), (64, 64), interpolation=cv2.INTER_CUBIC))
-            test_label_data.append(data_catalog)
-
-    train_data_set = np.array([train_image_raw_data, train_label_data])
-    train_data_set = train_data_set.transpose()
-
-    test_data_set = np.array([test_image_raw_data, test_label_data])
-    test_data_set = test_data_set.transpose()
-
-    x = np.array(train_data_set[:,0])
-    print('okok')
-    print(x.shape[0])
-    print(np.array(x[0]).shape)
-
-    np.random.shuffle(train_data_set)
-    np.random.shuffle(test_data_set)
-
-    return train_data_set, test_data_set
-
-
-
-def extract_class_num(file_path):
-    class_num = 0
-
-    for data_catalog in os.listdir(file_path + '/dataset/test'):
-        class_num = class_num + 1
-
-    return class_num
-
-def build_model_CNN_1():
+def build_model_AlexNet(normal_size, class_num):
+    # initialize the model
     model = Sequential()
+    inputShape = (normal_size, normal_size, 1)
 
-    model.add(InputLayer(input_shape=[64, 64, 1]))
-    model.add(Conv2D(filters=32, kernel_size=5, strides=1, padding='same', activation='relu'))
-    model.add(MaxPool2D(pool_size=5, padding='same'))
-
-    model.add(Conv2D(filters=50, kernel_size=5, strides=1, padding='same', activation='relu'))
-    model.add(MaxPool2D(pool_size=5, padding='same'))
-
-    model.add(Conv2D(filters=80, kernel_size=5, strides=1, padding='same', activation='relu'))
-    model.add(MaxPool2D(pool_size=5, padding='same'))
-
-    model.add(Dropout(0.25))
+    # first set of CONV => RELU => POOL layers
+    model.add(Conv2D(20, (5, 5), padding="same", input_shape=inputShape))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # second set of CONV => RELU => POOL layers
+    model.add(Conv2D(50, (5, 5), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # first (and only) set of FC => RELU layers
     model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='softmax'))
+    model.add(Dense(500))
+    model.add(Activation("relu"))
 
-    model.summary()
+    # softmax classifier
+    model.add(Dense(class_num))
+    model.add(Activation("softmax"))
 
+    # return the constructed network architecture
     return model
 
-def train_model(model_to_train, train_data):
-    x_train = np.array([i[0] for i in train_data]).reshape(-1, 64, 64, 1)
-    y_train = np.array([i[1] for i in train_data])
-    model_to_train.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-    model_to_train.fit(x=x_train, y=y_train, epochs=12,batch_size=20,validation_split=0.3,shuffle=True)
-    model_to_train.save('./generated_model/')
+def train_model(model_to_train, image_data, label_data, model_name):
+    print("Train-Validation Split")
+    train_x, vali_x, train_y, vali_y = train_test_split(image_data, label_data, test_size=0.2)
 
-def evaluate_model(model_to_evaluate, test_data):
-    x_test = np.array([i[0] for i in test_data]).reshape(-1, 64, 64, 1)
-    y_test = np.array([i[1] for i in test_data])
-    loss, accuracy = model_to_evaluate.evaluate(x_test, y_test)
-    print('test loss',loss)
-    print('test accuracy',accuracy)
+    lr_reducer = ReduceLROnPlateau(factor=0.005, cooldown=0, patience=5, min_lr=0.5e-6, verbose=1)
+    early_stopper = EarlyStopping(min_delta=0.001, patience=10, verbose=1)
+    checkpoint = ModelCheckpoint('./model/' + model_name + '_model.h5',monitor="val_acc", verbose=1,save_best_only=True, save_weights_only=True,mode="max")
+    model_to_train.compile(loss='binary_crossentropy', optimizer=Adam(lr=1e-3, decay=1e-3 / 30), metrics=['accuracy'])
+    model_to_train.fit(train_x, train_y, batch_size=64,
+              epochs=30,
+              validation_data=(vali_x, vali_y),
+              shuffle=True,
+              callbacks=[lr_reducer, early_stopper, checkpoint]
+              )
+    print("Finish training")
+
+def evaluate_model(model_to_evaluate, image_data, label_data):
+    loss, accuracy = model_to_evaluate.evaluate(image_data, label_data)
+
+    print('test loss;', loss)
+    print('test accuracy:', accuracy)
+
+
+def predict_image(image_path, model_path, model_name):
+    weights_path = model_path + model_name + '_model.h5'
+    predict_image_data = []
+
+    for each_image in os.listdir(image_path):
+        img_read = cv2.imread(image_path + each_image, cv2.IMREAD_GRAYSCALE)
+        img_read = cv2.resize(img_read, (64, 64), interpolation=cv2.INTER_CUBIC)
+        img_read = img_to_array(img_read)
+
+        predict_image_data.append(img_read)
+
+
+    predict_image_data = np.array(predict_image_data, dtype="float") / 255.0
+
+    model_to_predict = build_model_AlexNet(64, 2)
+    model_to_predict.load_weights(weights_path)
+    result = model_to_predict.predict_proba(predict_image_data)
+    print(result)
+    for each_result  in result:
+        if np.argmax(each_result) == 0:
+            print("cat with")
+            print(each_result[0])
+        else:
+            print("dog with")
+            print(each_result[1])
+
